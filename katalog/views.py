bdf73b57e1444f8bd2ca74103644f44faa8a3f39
Mahmud Asrul <mahmud.asrul@ui.ac.id>
2022-09-14 11:23:23 +0700

    Tugas 2

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
+from gettext import Catalog
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'list_barang' : data_barang_katalog,
+        'nama': 'Mahmud Asrul',
+        'npm': '2106655255'
+    }
+    return render(request, "katalog.html", context)
